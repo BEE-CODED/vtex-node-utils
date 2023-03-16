6990a4924f85d60aca0773d6f31dfaf7799365ec
Bogdan Duda <bogdan@beecoded.ro>
2023-03-16 21:02:01 +0200

    fix gulpfile location

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 // Include gulp
-const gulp = require('gulp');
+const gulpfile = require('gulp');
 const colors = require('gulp-colors'),
     run = require('gulp-run'),
     pjson = require('./package.json');
 
-gulp.task('tag:commit', function () {
+gulpfile.task('tag:commit', function () {
     const version = pjson.version;
     console.log(colors.bgWhite.black.bold(' \nTagging the version ' + version + ' \n'));
     return run('git tag ' + version).exec();
