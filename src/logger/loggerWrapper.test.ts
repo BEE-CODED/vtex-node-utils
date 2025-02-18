aa526b78a6d38faeeff8e1928df9e2a546bcb80e
Bogdan Duda <bogdan@beecoded.ro>
2025-02-18 11:06:22 +0200

    Update deps versions & bump minor

--- src/logger/loggerWrapper.test.ts
+++ src/logger/loggerWrapper.test.ts
@@ -3,86 +3,124 @@ import * as winston from 'winston'
 import {LoggerWrapper} from './loggerWrapper'
 import {VtexTransport} from './vtex-transport'
 
-import {IOContext, Logger, LogLevel} from '@vtex/api'
+import {IOContext, Logger} from '@vtex/api'
 
 interface BaseContext {
-  vtex: IOContext
+    vtex: IOContext
 }
 
 describe('logger wrapper', () => {
+    let context = {} as Partial<BaseContext['vtex']>, ctx = {} as BaseContext
+    let vtexLoggerMock: jest.SpyInstance
+    const numLoggers = 5 * 1000
 
-  beforeAll(cb => {
-    LoggerWrapper.initLogger('test-service-name', {level: 'debug', enableConsole: true})
-    cb()
-  })
 
-  beforeEach(cb => {
-    LoggerWrapper.initLogger('test-service-name', {level: 'debug', enableConsole: true})
-    cb()
-  })
+    beforeAll(cb => {
+        context = {
+            account: 'test-account',
+            workspace: 'test-workspace',
+            requestId: 'test-request-id',
+            operationId: 'test-operation-id',
+            production: false,
+        }
 
-  // it('should init logger with console transport', () => {
-  //   expect(logger.transports.length).toBeGreaterThanOrEqual(1)
-  //   expect(logger.transports.filter(t => t instanceof winston.transports.Console).length).toBe(1)
-  // })
+        ctx = {
+            vtex: {
+                ...context,
+                logger: new Logger(ctx as any),
+            },
+        } as BaseContext
 
-  // it('should init logger with no transports', () => {
-  //   expect(logger.transports.length).toBe(0)
-  // })
+        vtexLoggerMock = jest
+            .spyOn(ctx.vtex.logger, 'log')
+            .mockImplementation((message: string, level: string) => {
+                console.log('Mocked logger call', message, level)
+            })
 
-  it('should add VtexTransport to logger when getting named logger', cb => {
-    const context = {
-      account: 'test-account',
-      workspace: 'test-workspace',
-      requestId: 'test-request-id',
-      operationId: 'test-operation-id',
-      production: false,
-    }
 
-    const ctx = {
-      vtex: {
-        ...context,
-      },
-    } as BaseContext
-    ctx.vtex.logger = new Logger(ctx as any)
+        LoggerWrapper.initLogger('test-service-name', {level: 'debug', enableConsole: true})
 
-    const namedLogger = LoggerWrapper.getLogger(ctx, 'test-logger')
-    expect(namedLogger.baseLogger.transports.length).toBeGreaterThanOrEqual(1)
-    const vtexTransport = namedLogger.baseLogger.transports.find(t => t instanceof VtexTransport)
-    expect(vtexTransport).toBeDefined()
+        cb()
+    })
 
-    let lastVtexMessage: any
-    const vtexLoggerMock = jest
-        .spyOn(ctx.vtex.logger, 'log')
-        .mockImplementation((message: string, level: string) => {
-          // console.log('Mocked logger call', message, level)
-          lastVtexMessage = message
-        })
+    afterEach(cb => {
+        vtexLoggerMock.mockReset()
+        cb()
+    })
 
-    let lastConsoleMessage: any
-    const consoleLogMock = jest
-        .spyOn(winston.transports.Console.prototype, 'log')
-        .mockImplementation((message: string) => {
-          // console.log('Mocked Console call', message)
-          lastConsoleMessage = message
-        })
+    it('should init logger with console transport', () => {
+        const logger = LoggerWrapper.getLogger(ctx, 'test-logger')
+        expect(logger.baseLogger.transports.length).toBeGreaterThanOrEqual(1)
+        expect(logger.baseLogger.transports.filter(t => t instanceof winston.transports.Console).length).toBe(1)
+    })
 
-    namedLogger.debug('test message', {test: 'test'})
-    expect(vtexLoggerMock).toBeCalledTimes(1)
-    expect(consoleLogMock).toBeCalledTimes(1)
+    it('should init logger with vtex Logger', () => {
+        const logger = LoggerWrapper.getLogger(ctx, 'test-logger')
+        expect(logger.baseLogger.transports.length).toBeGreaterThanOrEqual(1)
+        expect(logger.baseLogger.transports.filter(t => t instanceof VtexTransport).length).toBe(1)
+    })
 
-    const parsedLastMessage = JSON.parse(lastVtexMessage)
-    expect(parsedLastMessage).toStrictEqual(expect.objectContaining({
-      message: 'test message',
-      test: 'test',
-      service: 'test-service-name',
-      name: 'test-logger',
-      level: 'debug',
-      ...context,
-    }))
-    expect(parsedLastMessage).toHaveProperty('timestamp')
-    expect(parsedLastMessage).toHaveProperty('level', 'debug')
+    it(`should not crash when getting ${numLoggers} loggers`, () => {
+        const loggers = []
+        for (let i = 0; i < numLoggers; i++) {
+            const logger = LoggerWrapper.getLogger(ctx, `test-logger-${i}`)
+            logger.info('test message ' + i)
+            loggers.push(logger)
+            if (i > 0) {
+                expect(loggers[i].baseLogger).toStrictEqual(loggers[0].baseLogger)
+            }
+        }
 
-    cb()
-  })
+        expect(ctx.vtex.logger.log).toBeCalledTimes(numLoggers)
+        expect(loggers.length).toBe(numLoggers)
+    })
+
+    // it('should init logger with no transports', () => {
+    //   expect(logger.transports.length).toBe(0)
+    // })
+
+    it('should add VtexTransport to logger when getting named logger', cb => {
+        const namedLogger = LoggerWrapper.getLogger(ctx, 'test-logger')
+        expect(namedLogger.baseLogger.transports.length).toBeGreaterThanOrEqual(1)
+
+        const vtexTransport = namedLogger.baseLogger.transports.find(t => t instanceof VtexTransport)
+        expect(vtexTransport).toBeDefined()
+
+        vtexLoggerMock.mockClear()
+        let lastVtexMessage: any
+        const localVtexLoggerMock = jest
+            .spyOn(ctx.vtex.logger, 'log')
+            .mockImplementation((message: string, level: string) => {
+                console.log('Mocked logger call', message, level)
+                lastVtexMessage = message
+            })
+
+        let lastConsoleMessage: any
+        const consoleLogMock = jest
+            .spyOn(winston.transports.Console.prototype, 'log')
+            .mockImplementation((message?: any, ...optionalParams: any[]) => {
+                console.log('Mocked Console call', message, optionalParams)
+                lastConsoleMessage = message
+            })
+
+        namedLogger.debug('test message', {test: 'test'})
+        expect(localVtexLoggerMock).toBeCalledTimes(1)
+        expect(consoleLogMock).toBeCalledTimes(1)
+
+        expect(localVtexLoggerMock).lastCalledWith(expect.any(String), 'debug')
+
+        const parsedLastMessage = JSON.parse(lastVtexMessage)
+        expect(parsedLastMessage).toStrictEqual(expect.objectContaining({
+            message: 'test message',
+            test: 'test',
+            service: 'test-service-name',
+            name: 'test-logger',
+            level: 'debug',
+            ...context,
+        }))
+        expect(parsedLastMessage).toHaveProperty('timestamp')
+        expect(parsedLastMessage).toHaveProperty('level', 'debug')
+
+        cb()
+    })
 })
